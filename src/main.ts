a92e9e23c3f108649c839df83fa226e5a66b79eb
ricky <zhangqingcq@foxmail.com>
2023-07-11 16:42:31 +0800

    vue pinia plug test

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-const _pinia = createPinia()
-app.use(_pinia)
+app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
